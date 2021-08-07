2526379b48000db4a9043b1e465291acd762bc8e
Matthew Blow <43573941+MatthewBlow@users.noreply.github.com>
2021-08-07 18:56:59 +0200

    Initial Upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-function App() {
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  { id: "e1", title: "PC setup", amount: 3530, date: new Date(2021, 2, 28) },
+  { id: "e2", title: "Car", amount: 3730, date: new Date(2021, 5, 28) },
+  { id: "e3", title: "House", amount: 4530, date: new Date(2021, 8, 28) },
+  { id: "e4", title: "Shoes", amount: 4353, date: new Date(2021, 3, 28) },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
+  }
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>This is visible</p>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses}/>
     </div>
   );
 }
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,34 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
+import ExpenseList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selectedYear={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses}/>
+        <ExpenseList items={filteredExpenses}/>
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
